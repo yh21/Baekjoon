c37ed78e2e0858190462efe3f3bd8ab47767f392
YunHyung Chang <yh_21@naver.com>
2025-06-18 22:06:19 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int num;
+    scanf("%d", &num);
+    
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == num)
+            sum++;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
