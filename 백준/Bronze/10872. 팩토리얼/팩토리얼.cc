2024e7d9fa167700a2c43e16633de5786a890803
YunHyung Chang <yh_21@naver.com>
2025-09-07 17:31:58 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int factorial = 1;
+    for (int i = 1; i <= n; i++){
+        factorial *= i;
+    }
+
+    printf("%d", factorial);
+    return 0;
+}
